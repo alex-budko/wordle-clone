dc20a1c816d58bd293c6d2220b46693392e5ba8b
alex-budko <xalex.budko@gmail.com>
2022-06-15 22:29:02 -0400

    trivial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import WordlePage from "./pages/WordlePage";
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [wordList, getWordList] = useState(null)
+  const [word, createWord] = useState(null)
+
+  useEffect(()=> {
+    async function fetchAPI() {
+      let response = await fetch('https://api.frontendexpert.io/api/fe/wordle-words')
+      let data = await JSON.parse(response)
+      getWordList(data)
+    }
+    fetchAPI() 
+  }, [])
+
+  function getWord() {
+    let i = getWordList.length * Math.random()
+    createWord(wordList[i])
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WordlePage getWord={getWord} />
     </div>
   );
 }
