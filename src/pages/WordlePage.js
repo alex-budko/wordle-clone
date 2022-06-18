75b1e59af867825ff8de98154c7e6477d6778bb3
alex-budko <xalex.budko@gmail.com>
2022-06-17 23:20:10 -0400

    working out async useState

--- src/pages/WordlePage.js
+++ src/pages/WordlePage.js
@@ -4,35 +4,39 @@ import WordleRow from '../components/WordleRow'
 
 const WordlePage = () => {
 
-    const [wordList, getWordList] = useState([])
     const [word, createWord] = useState('Alexa')
     const [guesses, setGuesses] = useState(Array(6).fill(''))
     const [curGuess, setCurGuess] = useState('')
     const [guessNum, setGuessNum] = useState(0)
 
     useEffect(()=>{
-        const handleKeyDown = event => {
+        const handleKeyDown = (event) => {
             const { key, keyCode } = event
-            if ((keyCode >= 65 && keyCode <= 90) && (curGuess.length <= 4)) {
-                
-                let newGuesses = guesses
-
+            if ((keyCode >= 65 && keyCode <= 90) && (curGuess.length <= 5)) {
                 setCurGuess(curGuess.concat(key.toUpperCase()))
-
-                newGuesses[guessNum] = curGuess
-
-                setGuesses(newGuesses)
+            } else {
+                setGuessNum(guessNum + 1)
+                setCurGuess('')
             }
         }
         window.addEventListener('keydown', handleKeyDown);
         return () => window.removeEventListener('keydown', handleKeyDown)
-    }, [guesses, curGuess, guessNum])
+    })
+
+    // useEffect(()=>{
+    //     setGuesses((guesses)=> {
+    //         guesses[guessNum] = curGuess
+
+    //     }
+    // }, [curGuess])
+
+
+    // useEffect(()=>
     
     return (
         <div>
             {
                 guesses.map((guess, index) => {
-                    console.log(guess)
                     return (
                         <div key={index} >
                             <WordleRow guess={ guess } />
