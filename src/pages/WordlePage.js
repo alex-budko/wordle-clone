d7c2730a02ecae330341bd94195ec55983790646
alex-budko <xalex.budko@gmail.com>
2022-06-19 16:56:30 -0400

    implemented lose/win conditions

--- src/pages/WordlePage.js
+++ src/pages/WordlePage.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { WordleWords } from '../components/data/WordleWords'
 import WordleRow from '../components/WordleRow'
 
 const WordlePage = () => {
 
-    const [word, createWord] = useState('ALEXA')
+    const [word, createWord] = useState('')
     const [guesses, setGuesses] = useState(Array(6).fill(''))
     const [curGuess, setCurGuess] = useState('')
     const [guessNum, setGuessNum] = useState(0)
     const [started, setStarted] = useState(false)
+    //const [end, endGame] = useState(false)
 
     useEffect(()=>{
         const handleKeyDown = (event) => {
@@ -20,12 +22,15 @@ const WordlePage = () => {
             } else if (keyCode === 8 && curGuess.length >= 1) {
                 setCurGuess(curGuess.slice(0, curGuess.length - 1))
                 //if 'ENTER' is pressed
-            } else if (keyCode === 13) {
-                if (curGuess.length === 5) {
+            } else if (keyCode === 13 && curGuess.length === 5 && guesses.length) {
+                //check winning conditions
+                if (word === curGuess) {
+                    console.log('Win')
+                } else if (guessNum === 5) {
+                    console.log('Lose')
+                } else {
                     setCurGuess('')
                     setGuessNum(guessNum + 1)
-                } else {
-                    return;
                 }
             }
         }
@@ -35,8 +40,11 @@ const WordlePage = () => {
 
     useEffect(()=>{
         if (!started) {
+            let n = Math.floor(Math.random() * WordleWords.length + 1)
+            createWord(WordleWords[n])
             setStarted(true)
         } else {
+            console.log(word)
             setGuesses(guesses => {
                 return [
                     ...guesses.slice(0, guessNum),
