e6a84417b00244251ab8f0d78e5f31ced507d05d
alex-budko <xalex.budko@gmail.com>
2022-06-26 22:32:39 -0400

    finished adjusting colors

--- src/pages/WordlePage.js
+++ src/pages/WordlePage.js
@@ -11,61 +11,57 @@ const WordlePage = () => {
     const [colors, setColors] = useState(Array(6).fill(Array(5).fill('white')))
     const [guessNum, setGuessNum] = useState(0)
     const [started, setStarted] = useState(false)
-
-    //const [end, endGame] = useState(false)
+    const [ended, setEnded] = useState(false)
 
     useEffect(()=>{
-        const handleKeyDown = (event) => {
-            const { key, keyCode } = event
-
-                //if alphabetical key is pressed
-            if ((keyCode >= 65 && keyCode <= 90) && (curGuess.length < 5)) {
-                setCurGuess(curGuess.concat(key.toUpperCase()))
-                //if 'BACKSPACE' is pressed
-            } else if (keyCode === 8 && curGuess.length >= 1) {
-                setCurGuess(curGuess.slice(0, curGuess.length - 1))
-                //if 'ENTER' is pressed
-            } else if (keyCode === 13 && curGuess.length === 5 && guesses.length) {
-                //check winning conditions
-                if (WordleWords.includes(curGuess)) {
-                    if (word === curGuess) {
-                        console.log('Win')
-                    } else if (guessNum === 5) {
-                        console.log('Lose')
-                    } else {
+        if (!ended) {
+            const handleKeyDown = (event) => {
+                const { key, keyCode } = event
+    
+                    //if alphabetical key is pressed
+                if ((keyCode >= 65 && keyCode <= 90) && (curGuess.length < 5)) {
+                    setCurGuess(curGuess.concat(key.toUpperCase()))
+                    //if 'BACKSPACE' is pressed
+                } else if (keyCode === 8 && curGuess.length >= 1) {
+                    setCurGuess(curGuess.slice(0, curGuess.length - 1))
+                    //if 'ENTER' is pressed
+                } else if (keyCode === 13 && curGuess.length === 5 && guesses.length) {
+                    //check winning conditions
+                    if (WordleWords.includes(curGuess)) {
                         setGuessNum(guessNum + 1)
+                        if (word === curGuess) {
+                            setEnded(true)
+                            console.log('Win')
+                        } else if (guessNum === 5) {
+                            setEnded(true)
+                            console.log('Lose')
+                        }
                     }
                 }
             }
+            window.addEventListener('keydown', handleKeyDown);
+            return () => window.removeEventListener('keydown', handleKeyDown)
         }
-        window.addEventListener('keydown', handleKeyDown);
-        return () => window.removeEventListener('keydown', handleKeyDown)
     })
 
     useEffect(()=> {
         for (let i = 0; i < 5; i++) {
-            console.log(colors[guessNum])
-            console.log(colors[guessNum][i])
             if (curGuess && curGuess[i] === word[i]) {
-                setColors(colors => {
-                    return [
-                        ...colors.slice(0, guessNum),
-                        colors[guessNum][i] = 'green',
-                        ...colors.slice(guessNum + 1),
-                    ]
-                })
+                setColors(colors => colors.map((colorRow, j) => 
+                    colorRow = guessNum-1===j ? [...colorRow.slice(0, i), ['#adff2f'], ...colorRow.slice(i + 1)] : colorRow
+                ))
             }
             else if (curGuess && word.includes(curGuess[i])) {
-                setColors(colors => {
-                    return [
-                        ...colors.slice(0, guessNum),
-                        colors[guessNum][i] = 'yellow',
-                        ...colors.slice(guessNum + 1),
-                    ]
-                })
+                setColors(colors => colors.map((colorRow, j) => 
+                colorRow = guessNum-1===j ? [...colorRow.slice(0, i), ['#ffd700'], ...colorRow.slice(i + 1)] : colorRow
+                ))
+            } else {
+                setColors(colors => colors.map((colorRow, j) => 
+                colorRow = guessNum-1===j ? [...colorRow.slice(0, i), ['#f0f8ff'], ...colorRow.slice(i + 1)] : colorRow
+                ))
             }
+            console.log(colors)
         }
-        console.log(colors)
         setCurGuess('')
     }, [guessNum])
 
